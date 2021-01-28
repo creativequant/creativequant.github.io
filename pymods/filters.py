5ed26e196a87b60247457db66efdb1fac0a93926
eracle <eracle@posteo.eu>
2021-01-28 12:16:24 +0100

    first commit

---
+++ pymods/filters.py
@@ -0,0 +1,50 @@
+from nbconvert.filters.markdown_mistune import MarkdownWithMath, IPythonRenderer
+
+
+class My_IPythonRenderer(IPythonRenderer):
+    def image(self, src, title, text):
+        """Rendering a image with title and text.
+
+        :param src: source link of the image.
+        :param title: title text of the image.
+        :param text: alt text of the image.
+        """
+        html = super().image(src, title, text)
+        return f"""
+
+                                     <span class="image main">{html}</span>"""
+
+    def header(self, text, level, raw=None):
+        html = '<h%d>%s</h%d>\n' % (level, text, level)
+        if level == 1:
+            html = f"""
+                                    <header class="main">
+                                        {html}                                    </header>
+            """
+        return html
+
+    def hrule(self):
+        """Rendering method for ``<hr>`` tag."""
+        if self.options.get('use_xhtml'):
+            return '<hr />\n'
+        return """
+                                     <hr class="major" />
+
+"""
+
+
+def my_markdown2html(source):
+    """Convert a markdown string to HTML using mistune"""
+    return MarkdownWithMath(renderer=My_IPythonRenderer(
+        escape=False)).render(source)
+
+
+def stripcssstyle(source):
+    """ Remove style tag from html"""
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(source, features="lxml")
+
+    for t in soup.find_all("style"):
+        t.replaceWith('')
+    return soup.html.body.div.prettify()
+
