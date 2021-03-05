c3a97a197653b1b055da0283e958d2dde26c026c
eracle <eracle@posteo.eu>
2021-03-05 21:31:56 +0100

    added google analytics

--- conf.py
+++ conf.py
@@ -1250,7 +1250,17 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8SWZG9T7QF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8SWZG9T7QF');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
